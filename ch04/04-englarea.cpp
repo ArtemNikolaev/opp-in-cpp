e2909890e77b50b4538d0f3c269590812dce86cc
Artem Nikolaev <artem@nikolaev.by>
2021-02-10 12:06:04 +0300

    ch04 04 - initialization

--- ch04/04-englarea.cpp
+++ ch04/04-englarea.cpp
@@ -11,11 +11,7 @@ struct Room {
 };
 
 int main() {
-    Room dining;
-    dining.length.feet = 13;
-    dining.length.inches = 6.5;
-    dining.width.feet = 10;
-    dining.width.inches = 0.0;
+    Room dining = { {13, 6.5}, {10, 0.0}};
 
     float l = dining.length.feet + dining.length.inches/12;
     float w = dining.width.feet + dining.width.inches/12;
